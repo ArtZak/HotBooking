e25fdf755eb0505c36068329a7f10e5577587700
ArtZak <artur_zaqaryan02@mail.ru>
2021-04-08 14:13:06 +0400

    initial search commit.

--- HotBooking/Controllers/HomeController.cs
+++ HotBooking/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using HotBooking.Domain;
+using HotBooking.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,52 @@ namespace HotBooking.Controllers
         public IActionResult Index()
         {
             return View(dataManager);
+        }
+
+
+        [HttpPost]
+        public IActionResult Search(string destination,
+                                    DateTime arrival,
+                                    DateTime departure,
+                                    int adults,
+                                    int children,
+                                    int rooms)
+        {
+            var countries = dataManager.Countries.GetAll();
+            IQueryable<City> cities;
+            List<Hotel> hotels = new List<Hotel>();
+
+            IQueryable<Country> selectedCountry = from t in countries where t.Title.ToLower() == destination.ToLower() select t;
+            if (selectedCountry.GetEnumerator().MoveNext() == true)
+            {
+                var _cities = selectedCountry.FirstOrDefault().Cities;
+                foreach (var city in _cities)
+                {
+                    hotels.AddRange(city.Hotels);
+                }
+            }
+            else
+            {
+                cities = dataManager.Cities.GetAll();
+                var selectedCity = from t in cities where t.Title.ToLower() == destination.ToLower() select t;
+
+                if (selectedCity.GetEnumerator().MoveNext() == false)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    hotels = selectedCity.FirstOrDefault().Hotels;
+                }
+            }
+
+            if(hotels == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityName = dataManager.Cities.GetById(hotels[0].CityId).Title;
+            return View(hotels);
         }
 
         public IActionResult Contacts()
---
+++ HotBooking/Models/SearchViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotBooking.Models
+{
+    public class SearchViewModel
+    {
+        [Required]
+        [Display(Name = "Destination")]
+        public string Destination { get; set; }
+
+        [Display(Name ="Arrival date")]
+        public DateTime ArrivalDate { get; set; }
+
+        [Display(Name = "Date of departure")]
+        public DateTime DateOfDeparture { get; set; }
+
+        [Display(Name = "Adults")]
+        public int Adults { get; set; }
+        
+        [Display(Name = "Children")]
+        public int Children { get; set; }
+
+        [Display(Name = "Rooms")]
+        public int Rooms { get; set; }
+    }
+}
